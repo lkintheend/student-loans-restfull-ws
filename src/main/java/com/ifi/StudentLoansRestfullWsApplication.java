55d2cad4680810067ce3e321704bb91b75a49c27
lkintheend <lkintheend@gmail.com>
2018-10-01 11:32:42 +0700

    1

---
+++ src/main/java/com/ifi/StudentLoansRestfullWsApplication.java
@@ -0,0 +1,12 @@
+package com.ifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoansRestfullWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLoansRestfullWsApplication.class, args);
+	}
+}
